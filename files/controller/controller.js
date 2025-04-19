93c6c143a65efa94ca81015ffc597c65392e6791
leevi-rinnetmaki <leevi.rinnetmaki@hotmail.com>
2025-04-19 20:41:58 +0300

    Added company filters

--- files/controller/controller.js
+++ files/controller/controller.js
@@ -1,8 +1,30 @@
 import { fetchData, postData, fetchRestaurants } from "../model/api.js";
-import { renderRestaurants } from "../view/restaurant.js";
+import { renderRestaurants, filterRestaurants, showRestaurantOrMenu  } from "../view/restaurant.js";
+const restaurantSearchButton = document.querySelector('#restaurant-search-button');
+const restaurantSearchBar = document.querySelector('#restaurant-search-bar');
+const companySelectorAll = document.querySelector('#company-all');
+const companySelectorSodexo = document.querySelector('#company-sodexo');
+const companySelectorCompass = document.querySelector('#company-compass-group');
 
-async function initialize() {
-    renderRestaurants(await fetchRestaurants());
+const apiRestaurants = await fetchRestaurants();
+async function initialize(restaurants) {
+    renderRestaurants(restaurants);
 }
+initialize(apiRestaurants);
 
-initialize();
+restaurantSearchButton.addEventListener('click', async () => {
+    const sear = restaurantSearchBar.value;
+    console.log(sear);
+})
+
+companySelectorAll.addEventListener('click', () => {
+    filterRestaurants(apiRestaurants, 'all');
+});
+
+companySelectorSodexo.addEventListener('click', () => {
+    filterRestaurants(apiRestaurants, 'Sodexo');
+});
+
+companySelectorCompass.addEventListener('click', () => {
+    filterRestaurants(apiRestaurants, 'Compass Group');
+});
--- files/view/restaurant.js
+++ files/view/restaurant.js
@@ -2,16 +2,37 @@ import { fetchRestaurants, fetchWeeklyMenu } from "../model/api.js";
 
 const restaurantButton = document.querySelector('#restaurant-button');
 const mainRestaurant = document.querySelector('#main-restaurant');
+const companySelector = document.querySelector('#company-selector');
+
 const daySelector = document.querySelector('#day-selector');
+const daySelectorButtons = document.querySelectorAll('#day-selector button');
 const menuList = document.querySelector('#menu-list');
 const restaurantList = document.querySelector('#restaurant-list');
+const restaurantListList = document.querySelectorAll('#restaurant-list li');
 
+const displayRestaurants = document.querySelectorAll('.display-restaurants');
+const displayMenu = document.querySelectorAll('.display-menu');
+
+function showRestaurantOrMenu(boole){
+    if(boole){
+        displayRestaurants.forEach(element => {
+            element.style.display = 'block';
+        });
+        displayMenu.forEach(element => {
+            element.style.display = 'none';
+        });
+    }else{
+        displayRestaurants.forEach(element => {
+            element.style.display = 'none';
+        });
+        displayMenu.forEach(element => {
+            element.style.display = 'block';
+        });
+    }
+}
 
 async function renderRestaurants(restaurants){
-    restaurantButton.style.display = 'none';
-    daySelector.style.display = 'none';
-    menuList.style.display = 'none';
-    restaurantList.style.display = 'block';
+    showRestaurantOrMenu(true);
     restaurantList.innerHTML = '';
 
     restaurants.forEach(restaurant => {
@@ -19,12 +40,19 @@ async function renderRestaurants(restaurants){
         restaurantItem.classList.add('restaurant-item');
         restaurantItem.id = restaurant._id;
         restaurantItem.innerHTML = `
-            <h2>${restaurant.name}</h2>
-            <p>Address: ${restaurant.address}</p>
+            <h2>${restaurant?.name}</h2>
+            <p>City: ${restaurant?.city}</p>
+            <p>Postal Code: ${restaurant?.postalCode}</p>
+            <p>Address: ${restaurant?.address}</p>
+            <p>Phone: ${restaurant?.phone}</p>
+            <p>Company: ${restaurant?.company}</p>
+            <p>id: ${restaurant?._id}</p>
         `;
         const menuButton = document.createElement('button');
         menuButton.classList.add('menuButton');
-        menuButton.id = `restaurant-${restaurant._id}`;
+        //menuButton.id = `restaurant-${restaurant._id}`;
+        menuButton.id = restaurant._id;
+
         menuButton.textContent = 'View Menu';
         menuButton.addEventListener('click', () => {
             renderMenu(restaurant._id);
@@ -35,10 +63,8 @@ async function renderRestaurants(restaurants){
 }
 
 async function renderMenu(restaurantId) {
-    restaurantButton.style.display = 'block';
-    daySelector.style.display = 'block';
-    menuList.style.display = 'block';
-    restaurantList.style.display = 'none';
+
+    showRestaurantOrMenu(false);
     menuList.innerHTML = '';
     console.log(`Clicked on restaurant ${restaurantId}`);
     const menu = await fetchWeeklyMenu(restaurantId);
@@ -47,15 +73,27 @@ async function renderMenu(restaurantId) {
     console.log(menu.days[4].date);
 
     restaurantButton.addEventListener('click', () => {
-        restaurantButton.style.display = 'none';
-        daySelector.style.display = 'none';
-        menuList.style.display = 'none';
-        restaurantList.style.display = 'block';
+        showRestaurantOrMenu(true);
     });
+}
 
+function filterRestaurants(restaurants, company){
+    //const restaurantListList = document.querySelectorAll('#restaurant-list li');
 
-
+    if(company==='all'){
+        renderRestaurants(restaurants);
+    }else{
+        const filteredRestaurants = [];
+        restaurants.forEach((restaurant) =>{
+            if(restaurant.company === company){
+                filteredRestaurants.push(restaurant);
+            }
+        })
+        renderRestaurants(filteredRestaurants);
+    };
 }
 
 
-export { renderRestaurants };
+
+
+export { renderRestaurants, filterRestaurants, showRestaurantOrMenu };
